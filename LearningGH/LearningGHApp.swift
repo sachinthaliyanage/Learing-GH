a89f10bab618b9ebdf75bb6b81753cffaeda8ebb
Sachintha Liyanage <sachintha.li0417@gmail.com>
2022-09-23 22:59:26 +0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by sachintha on 2022-09-23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
